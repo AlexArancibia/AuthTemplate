210df2cedd2c3fdb07fcca40ec6aea39f58f55ec
Alexander Arancibia <alexander@MacBook-Pro-de-Alexander.local>
2025-08-13 07:19:17 -0500

    gtm added

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,6 +94,18 @@ export default async function RootLayout({
   return (
     <html lang="es">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W9VCKT6J');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        
         {/* Structured Data */}
         <script
           type="application/ld+json"
@@ -113,6 +125,17 @@ export default async function RootLayout({
         />
       </head>
       <body className={poppins.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W9VCKT6J"
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <Toaster position="top-center" richColors />
         <Navbar user={session?.user} />
         <main className="min-h-screen">{children}</main>
